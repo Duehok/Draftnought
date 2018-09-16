8c8acd5d14e5d66488577b4c2d9e5d1d146f3ae1
Duehok <36293985+Duehok@users.noreply.github.com>
2018-09-16 22:01:27 +0200

    zoom on top view

--- window/sideview.py
+++ window/sideview.py
@@ -1,7 +1,7 @@
 """Side view display of the ship"""
 
 import tkinter as tk
-from PIL import ImageTk
+from PIL import Image, ImageTk
 
 _WIDTH = 700
 
@@ -22,14 +22,18 @@ class SideView(tk.Frame):
     def __init__(self, parent, ship_data):
         super().__init__(parent)
         if ship_data.side_pict is not None:
+            self._ship_data = ship_data
             self._image = ImageTk.PhotoImage(ship_data.side_pict)
+            self._ratio = 1.0
             self._canvas = tk.Canvas(self, width=_WIDTH, height=self._image.height())
-            self._canvas.create_image((0, 0), image=self._image, anchor=tk.NW)
+            self._pict_id = self._canvas.create_image((0, 0), image=self._image, anchor=tk.NW)
             self._canvas.grid()
             self._canvas.bind("<Motion>", self._on_move)
             self._canvas.bind("<ButtonPress-1>", self._on_click)
             self._canvas.bind("<ButtonRelease-1>", self._on_unclick)
             self._left_button_down = False
+
+            self._canvas.bind("<MouseWheel>", self._on_mousewheel)
 
     def _on_click(self, event):
         self._left_button_down = True
@@ -41,3 +45,14 @@ class SideView(tk.Frame):
     def _on_move(self, event):
         if self._left_button_down:
             self._canvas.scan_dragto(event.x, event.y, gain=1)
+
+    def _on_mousewheel(self, event):
+        if event.delta >0:
+            self._ratio = self._ratio*1.05
+        else:
+            self._ratio = self._ratio*0.95
+        self._canvas.delete(self._pict_id)
+        new_size = [round(coord*self._ratio) for coord in self._ship_data.side_pict.size]
+        self._image = ImageTk.PhotoImage(self._ship_data.side_pict.resize(new_size))
+        self._pict_id = self._canvas.create_image((0, 0), image=self._image, anchor=tk.NW)
+
