026d6df8fd457b7ff1f913b5fe3bb599289f92f6
Duehok <36293985+Duehok@users.noreply.github.com>
2018-09-19 22:04:13 +0200

    Build scripts includes readme

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ rmdir .\dist /s /q
 rmdir .\build /s /q
 pyinstaller main.py -F --name="Draftnought"
 COPY icon.ico .\dist\icon.ico
+COPY "README.md" ".\dist\README.md"
 COPY LICENSE .\dist\LICENSE
 xcopy /s /i ".\data" ".\dist\data"
